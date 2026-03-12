dad12c5026216065be5255ed60203a3dba4bd4d8
roarrioj <114079118+roarrioj@users.noreply.github.com>
2026-03-12 13:47:20 -0400

    AMBA

--- terraform.tf
+++ terraform.tf
@@ -21,9 +21,15 @@ terraform {
   backend "azurerm" {}
 }
 
+
 provider "alz" {
   library_overwrite_enabled = true
+
   library_references = [
+    {
+      path = "platform/amba"
+      ref  = "platform/amba/2026.01.1"
+    },
     {
       custom_url = "${path.root}/lib"
     }
